e91065360cce1add6fcd5df88beb6982eb2e2ddd
Carlos Henrique <ichfs9108@gmail.com>
2024-09-20 00:17:09 -0300

    Inserindo os dados iniciais da aplicacao

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
@@ -1,12 +1,6 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -48,9 +42,8 @@ public class Validation {
         updatedAt = now;
     }
 
-    @PrePersist
+    @PreUpdate
     public void preUpdate() {
         updatedAt = LocalDateTime.now();
     }
-
 }
